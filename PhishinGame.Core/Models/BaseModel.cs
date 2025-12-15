7d2de96a118076cbcb78c8549e14342ce99d275a
Thimo Bosma <thimo.bosma@windesheim.nl>
2025-12-15 09:06:44 -0800

    gewerkt aan EmailCompositionState

--- PhishinGame.Core/Models/BaseModel.cs
+++ PhishinGame.Core/Models/BaseModel.cs
@@ -1,6 +1,17 @@
-﻿namespace PhishingGame.Core.Models;
+﻿using System.Runtime.CompilerServices;
+
+namespace PhishingGame.Core.Models;
 
 public class BaseModel
 {
-    public Guid? Id { get; set; }
+    private Guid _id;
+    public Guid Id
+    {
+        get
+        {
+            if (_id == Guid.Empty) _id = Guid.NewGuid(); 
+            return _id;
+        }
+        set => _id = value;
+    }
 }
