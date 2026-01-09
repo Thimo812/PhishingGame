ae3fa73519d71996d69c3f2a0026da21e82910dc
Thimo Bosma <thimo.bosma@windesheim.nl>
2026-01-09 11:39:50 +0100

    dbcontext factory added

---
+++ PhishingGame.Data/PhishingDbContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace PhishingGame.Data;
+
+public class PhishingDbContextFactory : IDesignTimeDbContextFactory<PhishingDbContext>
+{
+    public PhishingDbContext CreateDbContext(string[] args)
+    {
+        var options = new DbContextOptionsBuilder<PhishingDbContext>()
+            .UseSqlServer(string.Empty)
+            .Options;
+
+        return new PhishingDbContext(options);
+    }
+}
